866305573d84ec368589ddc74d1249bc176bcaa1
sloan <sloan@pobox.com>
2016-10-07 11:57:01 -0700

    Initial commit.  Code complete but needs better docs

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var Logger = require('../index.js');
+
+Logger.setLevels({'all' : 'debug'});
+
+var log = Logger.getLogger('testClass');
+// This should work since all loggers default to info
+log.info("This is an info");
+// Though we set level, the logger wasn't registered at the time, so this fails. Which is wrong.
+log.debug("This is a debug");
